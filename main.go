501e931823e07751a4dccad233570270e61d9e0b
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-06-16 15:38:01 +0900

    前置演算子のparse
    p68

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s This is the Monkey programming language !", user.Username)
+	fmt.Printf("Hello %s This is the Monkey programming language! ", user.Username)
 	fmt.Printf("%s", "Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }

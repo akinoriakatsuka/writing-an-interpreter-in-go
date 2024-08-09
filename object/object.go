286a67d0690c025674eb245937a004da2119ca39
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-08-09 21:56:19 +0900

    return文の評価を修正
    内側のreturnが有効な時に優先されるようにする p149

--- object/object.go
+++ object/object.go
@@ -5,10 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
-	RETURN_VALUE_OBJECT = "RETURN_VALUE"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -38,5 +38,6 @@ func (n *Null) Inspect() string  { return "null" }
 type ReturnValue struct {
 	Value Object
 }
-func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJECT }
-func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }

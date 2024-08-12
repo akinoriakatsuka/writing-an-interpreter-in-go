8f7169baccfa97de289d7550879203bbf6f7fddb
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-08-12 10:11:21 +0900

    let文のテストを追加 p155

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -180,6 +180,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -195,6 +199,22 @@ func TestErrorHandling(t *testing.T) {
 			t.Errorf("wrong error message. expected=%q, got=%q",
 				tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatement(t *testing.T) {
+	tests := []struct{
+		input string
+		expected int64
+	}{
+		{"let a = 5; a", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
 
